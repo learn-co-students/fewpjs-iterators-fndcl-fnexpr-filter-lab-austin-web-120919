534dde3319f37740baf14ecee15148925b8cb22b
gwhitlock8 <gavinswhitlock@gmail.com>
2020-02-03 14:35:51 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array,string){
+   return array.filter(word => (word.toLowerCase() === string.toLowerCase()));
+}
+
+function fuzzyMatch(array,string){
+    return array.filter()
+}
+
+function fuzzyMatch(array,string){
+    return array.filter(word => {
+        let len = string.length;
+        if (word.slice(0,len) == string){
+            return word;
+        }
+    });
+}
+
+function matchName(array,string){
+    return array.filter( word => word.name == string)
+}
